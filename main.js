c04a0d9581630680263640a022dbf1a9aa321ebd
irem-koc <irem_koc_75@hotmail.com>
2022-10-12 14:23:21 +0300

    added info cards

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let variable_deletAll = 0;
+let al = document.querySelector(".alerticin")
+let first_card_body = document.querySelectorAll(".card-body")[0]
+let second_card_body = document.querySelectorAll(".card-body")[1]
 let ul = document.querySelector("ul.list-group");
 let hr = document.querySelector("#that")
 let li = document.querySelectorAll("li#li")
@@ -7,9 +10,22 @@ let icon_close = document.querySelector("i.fa.fa-close")
 let butonlar = document.querySelector(".btn").addEventListener("click", function (e) {
     e.preventDefault()
 })
-
+function showInfo(type, msg){
+//     <div class="alert alert-primary" role="alert">
+//   A simple primary alert—check it out!
+// </div>
+    let div = document.createElement("div");
+    div.className=`alert alert-${type}`;
+    div.textContent=msg;
+    first_card_body.append(div)
+    setTimeout(function(){
+        div.remove()
+        
+    },2000) 
+}
 function add() {
     if (input_text.value.trim()) {
+        setTimeout(showInfo("success", `Başarıyla eklendi \'${input_text.value}'`),2000) ;
         let liste_item = document.createElement("li");
         let i = document.createElement("i");
         liste_item.className = "list-group-item d-flex justify-content-between align-items-center";
@@ -20,6 +36,9 @@ function add() {
         liste_item.appendChild(i);
         ul.appendChild(liste_item);
         input_text.value = "";
+    }
+    else{
+        showInfo("danger", "Boş bir değer gönderme")
     }
 
 }
